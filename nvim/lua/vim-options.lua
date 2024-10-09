09ff01642825c4340ffeed773645c311f3acaf1f
devkaare <kaarehofferdal@gmail.com>
2024-10-10 00:21:31 +0200

    Upload files

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,26 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+--vim.opt.signcolumn = "number"
+vim.opt.scrolloff = 8
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+vim.wo.number = true
