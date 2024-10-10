2aeac2f2ab1aa20e3352c6c449a325f1ace935f6
devkaare <kaarehofferdal@gmail.com>
2024-10-10 02:07:43 +0200

    Upload files

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+    # Download oh my bosh to /usr/bin
+    oh-my-posh init fish --config ~/.posh/catppuccin.omp.json| source
+end
