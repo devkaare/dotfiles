cf2a5d946aaa332ec7cb5fba4f5fb1db76bafb71
devkaare <kaarehofferdal@gmail.com>
2025-02-18 06:06:46 +0100

    Feat

---
+++ .config/fish/functions/gitcmake.fish
@@ -0,0 +1,4 @@
+function gitcmake --wraps='/usr/bin/git --git-dir=/home/user1/go/src/github.com/devkaare/go-makefile/.git/ --work-tree=.' --description 'alias gitcmake=/usr/bin/git --git-dir=/home/user1/go/src/github.com/devkaare/go-makefile/.git/ --work-tree=.'
+  /usr/bin/git --git-dir=/home/user1/go/src/github.com/devkaare/go-makefile/.git/ --work-tree=. $argv
+        
+end
