bfe69680f58543e4ad81e4a73cda01de5ad99901
devkaare <kaarehofferdal@gmail.com>
2025-02-19 19:07:36 +0100

    Feat

---
+++ .config/fish/functions/gitpl.fish
@@ -0,0 +1,4 @@
+function gitpl --wraps='git pull origin main' --description 'alias gitpl=git pull origin main'
+  git pull origin main $argv
+        
+end
---
+++ .config/fish/functions/gitplf.fish
@@ -0,0 +1,4 @@
+function gitplf --wraps='git pull origin main -f' --description 'alias gitplf=git pull origin main -f'
+  git pull origin main -f $argv
+        
+end
