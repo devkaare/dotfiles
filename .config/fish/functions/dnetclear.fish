d0771ea6d034e057616dbe430c32dc53f8300a44
devkaare <kaarehofferdal@gmail.com>
2025-03-08 03:13:33 +0100

    Feat

--- .config/fish/functions/dnetclear.fish
+++ .config/fish/functions/dnetclear.fish
@@ -1,4 +1,4 @@
-function dnetclear --wraps=docker\ network\ rm\ -f\ 0feb58b5df6c\n468dda3fa9f7\n67a112a9a0f0 --wraps='docker network rm -f $(docker network ls -q)' --description 'alias dnetclear=docker network rm -f $(docker network ls -q)'
+function dnetclear --wraps='docker rm (docker ps -a -q) && docker volume rm (docker volume ls -q) && docker network rm -f $(docker network ls -q)' --wraps='docker rm (docker ps -a -q) && docker network rm -f $(docker network ls -q)' --wraps='docker rm (docker ps -a -q) && volume rm (docker volume ls -q) && docker network rm -f $(docker network ls -q)' --wraps='docker network rm -f $(docker network ls -q)' --description 'alias dnetclear=docker network rm -f $(docker network ls -q)'
   docker network rm -f $(docker network ls -q) $argv
         
 end
