c2844e4c95ad20341c9df77e0c176c5dcebfa419
devkaare <kaarehofferdal@gmail.com>
2025-03-22 23:07:32 +0100

    Feat

--- .config/fish/functions/dnetclear.fish
+++ .config/fish/functions/dnetclear.fish
@@ -1,4 +1,4 @@
-function dnetclear --wraps='docker rm (docker ps -a -q) && docker volume rm (docker volume ls -q) && docker network rm -f $(docker network ls -q)' --wraps='docker rm (docker ps -a -q) && docker network rm -f $(docker network ls -q)' --wraps='docker rm (docker ps -a -q) && volume rm (docker volume ls -q) && docker network rm -f $(docker network ls -q)' --wraps='docker network rm -f $(docker network ls -q)' --description 'alias dnetclear=docker network rm -f $(docker network ls -q)'
+function dnetclear --wraps='docker rm (docker ps -a -q); docker volume rm (docker volume ls -q); docker network rm -f $(docker network ls -q)' --wraps='docker rm (docker ps -a -q); docker network rm -f $(docker network ls -q)' --wraps='docker rm (docker ps -a -q); volume rm (docker volume ls -q); docker network rm -f $(docker network ls -q)' --wraps='docker network rm -f $(docker network ls -q)' --description 'alias dnetclear=docker network rm -f $(docker network ls -q)'
   docker network rm -f $(docker network ls -q) $argv
         
 end
--- .config/fish/functions/gitacp.fish
+++ .config/fish/functions/gitacp.fish
@@ -1,4 +1,4 @@
-function gitacp --wraps='git add . && git commit -m "Feat" && git push origin' --description 'alias gitacp=git add . && git commit -m "Feat" && git push origin'
-  git add . && git commit -m "Feat" && git push origin $argv
+function gitacp --wraps='git add .; git commit -m "Feat"; git push origin' --description 'alias gitacp=git add .; git commit -m "Feat"; git push origin'
+  git add .; git commit -m "Feat"; git push origin $argv
         
 end
